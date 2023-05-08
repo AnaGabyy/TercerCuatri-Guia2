6a3472d9df48f59c2ad056690898732adea5cf36
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 10:47:39 -0300

    Clase CuentaInversion que extiende de Cuenta

---
+++ TercerCuatri-Guia2/src/ejercicioUno/CuentaInversion.java
@@ -0,0 +1,8 @@
+package ejercicioUno;
+
+public class CuentaInversion extends Cuenta{
+
+    public CuentaInversion(Double saldo, String titular, Integer numeroCuenta) {
+        super(saldo, titular, numeroCuenta);
+    }
+}

5c0ef2d3625811a37823bb9254d4ac2beb275aa6
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 10:47:04 -0300

    Clase CuentaAhorro que extiende de Cuenta

---
+++ TercerCuatri-Guia2/src/ejercicioUno/CuentaAhorro.java
@@ -0,0 +1,8 @@
+package ejercicioUno;
+
+public class CuentaAhorro extends Cuenta{
+
+    public CuentaAhorro(Double saldo, String titular, Integer numeroCuenta) {
+        super(saldo, titular, numeroCuenta);
+    }
+}

f51a220d0f5de3317033e3568d13d854bb381e7f
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 10:48:20 -0300

    Clase SistemaBancario (Singleton)

---
+++ TercerCuatri-Guia2/src/ejercicioUno/SistemaBancario.java
@@ -0,0 +1,42 @@
+package ejercicioUno;
+
+import java.util.ArrayList;
+
+public class SistemaBancario {
+    private static SistemaBancario instance = null;
+    private ArrayList<Cuenta> cuentas;
+
+    /*El constructor es privado para garantizar que solo se pueda crear una instancia
+    de la clase a través del método estático "getInstance()"*/
+    private SistemaBancario() {
+        cuentas = new ArrayList<Cuenta>();
+    }
+
+    public static SistemaBancario getInstance() {
+        if (instance == null) {
+            instance = new SistemaBancario();
+        }
+        return instance;
+    }
+
+    public void agregarCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
+
+    public void eliminarCuenta(Cuenta cuenta) {
+        cuentas.remove(cuenta);
+    }
+
+    public ArrayList<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public Cuenta buscarCuenta(Integer numero) {
+        for (Cuenta cuenta : cuentas) {
+            if (cuenta.getNumeroCuenta() == numero) {
+                return cuenta;
+            }
+        }
+        return null;
+    }
+}

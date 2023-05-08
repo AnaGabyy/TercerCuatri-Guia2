7647c45cbc4b46c16d531524d0b2a7dad26fa422
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 10:47:57 -0300

    Interfaz CuentaBancaria

---
+++ TercerCuatri-Guia2/src/ejercicioUno/CuentaBancaria.java
@@ -0,0 +1,8 @@
+package ejercicioUno;
+
+public interface CuentaBancaria {
+
+    public void extraer(Double monto);
+    public void depositar(Double monto);
+    public void transferir(Cuenta cuentaOrigen, Cuenta cuentaDestino, Double monto);
+}

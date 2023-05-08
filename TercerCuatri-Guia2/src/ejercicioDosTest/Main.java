940fc29858b3db88ad63f7088596af005fa10c97
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 11:35:34 -0300

    clase Main

---
+++ TercerCuatri-Guia2/src/ejercicioDosTest/Main.java
@@ -0,0 +1,16 @@
+package ejercicioDosTest;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*Se desea implementar un sistema de gestión de envíos de paquetes. Los paquetes pueden ser de diferentes
+        tipos, como por ejemplo, paquetes pequeños, paquetes medianos, paquetes grandes, entre otros. Para representar
+        cada tipo de paquete, se definen interfaces como Package, Transportar, Fragil, RadioActivo, etc. Además, se
+        desea que el sistema sea implementado utilizando el patrón singleton para garantizar que solo haya una
+        instancia del sistema en ejecución. Finalmente, se requiere que los paquetes sean almacenados en una colección
+        de tipo ArrayList y que se utilice el polimorfismo para manejar los diferentes tipos de paquetes.*/
+
+
+
+    }
+}

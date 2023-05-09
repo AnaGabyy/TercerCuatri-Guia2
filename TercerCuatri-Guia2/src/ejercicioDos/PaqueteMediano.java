b99ff17a6495d3a4847d9e6bafb7f13500174547
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 10:30:24 -0300

    implementacion de la interfaz Fragil con sus metodos

--- TercerCuatri-Guia2/src/ejercicioDos/PaqueteMediano.java
+++ TercerCuatri-Guia2/src/ejercicioDos/PaqueteMediano.java
@@ -1,8 +1,13 @@
 package ejercicioDos;
 
-public class PaqueteMediano extends Paquete{
+public class PaqueteMediano extends Paquete implements  Fragil{
 
-    public PaqueteMediano(Integer idPaquete, Integer ancho, Integer alto) {
-        super(idPaquete, ancho, alto);
+    public PaqueteMediano(Integer idPaquete, Double peso, String destino) {
+        super(idPaquete, peso, destino);
+    }
+
+    @Override
+    public boolean esFragil() {
+        return true;
     }
 }

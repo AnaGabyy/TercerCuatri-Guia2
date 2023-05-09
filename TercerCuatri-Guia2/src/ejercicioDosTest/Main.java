640448ad2bbe4fb17b06aa0ecc42b79a43a5f839
AnaGabyy <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 10:33:11 -0300

    Prueba de objetos

--- TercerCuatri-Guia2/src/ejercicioDosTest/Main.java
+++ TercerCuatri-Guia2/src/ejercicioDosTest/Main.java
@@ -1,5 +1,7 @@
 package ejercicioDosTest;
 
+import ejercicioDos.*;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,6 +12,21 @@ public class Main {
         instancia del sistema en ejecución. Finalmente, se requiere que los paquetes sean almacenados en una colección
         de tipo ArrayList y que se use el polimorfismo para manejar los diferentes tipos de paquetes.*/
 
+        SistemaEnvio sistemaEnvio = SistemaEnvio.getInstance();
+
+        PaquetePequenio paquetePequenio = new PaquetePequenio(1, 3.3d, "Argentina");
+        PaqueteMediano paqueteMediano = new PaqueteMediano(2, 1.6d, "Brasil");
+        PaqueteGrande paqueteGrande = new PaqueteGrande(3, 9.0d, "Venezuela");
+
+        sistemaEnvio.agregarPaquete(paquetePequenio);
+        sistemaEnvio.agregarPaquete(paqueteMediano);
+        sistemaEnvio.agregarPaquete(paqueteGrande);
+
+        System.out.println(sistemaEnvio.cantidadPaquetes());
+
+        System.out.println(sistemaEnvio.pesoTotal());
+
+        sistemaEnvio.imprimirDetalles();
 
 
     }

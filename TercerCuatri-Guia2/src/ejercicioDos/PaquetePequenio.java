5af8cb9e11ed040e950185d5dbda5b131bbb1e82
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 02:04:07 -0300

    Clase PaquetePequenio que extiende de Paquete

---
+++ TercerCuatri-Guia2/src/ejercicioDos/PaquetePequenio.java
@@ -0,0 +1,8 @@
+package ejercicioDos;
+
+public class PaquetePequenio extends Paquete{
+
+    public PaquetePequenio(Integer idPaquete, Integer ancho, Integer alto) {
+        super(idPaquete, ancho, alto);
+    }
+}

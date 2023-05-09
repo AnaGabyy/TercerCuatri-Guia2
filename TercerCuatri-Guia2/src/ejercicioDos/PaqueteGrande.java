b8fad15913cf63aab8eff4be5af8fe4866d97883
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-09 02:03:28 -0300

    Clase PaqueteGrande que extiende de Paquete

---
+++ TercerCuatri-Guia2/src/ejercicioDos/PaqueteGrande.java
@@ -0,0 +1,8 @@
+package ejercicioDos;
+
+public class PaqueteGrande extends Paquete{
+
+    public PaqueteGrande(Integer idPaquete, Integer ancho, Integer alto) {
+        super(idPaquete, ancho, alto);
+    }
+}
